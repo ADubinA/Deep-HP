5a816074160638f2a34441b44700e27deac282c4
Almog Dubin <almogdubin@gmail.com>
2021-03-09 01:59:55 +0200

    added clustering

--- classical_registration/create_atlas.py
+++ classical_registration/create_atlas.py
@@ -1,10 +1,12 @@
 # examples/Python/Advanced/interactive_visualization.py
 
+import os, glob
 import numpy as np
 import copy
 import open3d as o3d
 import tqdm
-
+import random
+from sklearn.cluster import AgglomerativeClustering
 
 def lineseg_dists(p, a, b):
     # TODO for you: consider implementing @Eskapp's suggestions
@@ -102,7 +104,6 @@ def remove_non_normals(pcd,bone_line, rad=20):
     #
     #     if point_fitness<0.5:
     #         np.asarray(bone_pcd.colors)[point_index] = [1,0,0]
-    o3d.visualization.draw_geometries([bone_pcd])
     return bone_pcd
 
 def remove_non_on_tangent(pcd,bone_line, rad=10):
@@ -135,12 +136,32 @@ def check_full_bone(pcd,bone_line):
                       np.asarray(pcd.points)[point_index] - bone_line/2)
         if (dists<1).sum()>5:
             np.asarray(pcd.colors)[point_index] = [1,0,0]
-
-
-
-
     o3d.visualization.draw_geometries([pcd])
-def test1_consensus_class():
+
+def get_bone_groups(pcd, bone_line, cluster_dist = 20):
+    bone_tree = o3d.geometry.KDTreeFlann(pcd)
+    choosen_points = []
+    choosen_indexes = []
+    for point_index in tqdm.tqdm(range(np.asarray(pcd.points).shape[0])):
+        [k, nie, _] = bone_tree.search_radius_vector_3d(pcd.points[point_index], np.linalg.norm(bone_line) / 2)
+        if len(nie) < 5:
+            continue
+
+        dists = lineseg_dists(np.asarray(pcd.points)[nie[1:], :],
+                              np.asarray(pcd.points)[point_index] + bone_line / 2,
+                              np.asarray(pcd.points)[point_index] - bone_line / 2)
+        if (dists < 1).sum() > np.linalg.norm(bone_line) / 10:
+            np.asarray(pcd.colors)[point_index] = [1, 0, 0]
+            choosen_points.append(np.asarray(pcd.points)[point_index])
+            choosen_indexes.append(point_index)
+
+    choosen_points = np.array(choosen_points)
+    choosen_indexes = np.array(choosen_indexes)
+    clustering = AgglomerativeClustering(n_clusters=None,distance_threshold=cluster_dist, linkage="single").fit(choosen_points)
+    colors = np.random.random((clustering.labels_.shape[0],3))
+    np.asarray(pcd.colors)[choosen_indexes] = colors[clustering.labels_]
+    o3d.visualization.draw_geometries([pcd])
+def test1_get_some_bones():
     normal_rad = 10
     source = o3d.io.read_point_cloud(r"D:\visceral\full_skeletons\102946_CT_Wb.ply")
     numpy_source = np.asarray(source.points)
@@ -154,10 +175,41 @@ def test1_consensus_class():
     source.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=normal_rad, max_nn=30))
     points = pick_points(source)
     bone_line = np.asarray(source.points)[points[0]] - np.asarray(source.points)[points[1]]
+    print(bone_line)
     pcd = remove_non_normals(source,bone_line)
     check_full_bone(pcd,bone_line)
     # pcd = remove_interior(source)
     # remove_non_normals(pcd, bone_line)
+def test2_legs():
+    skeleton_folder = r"D:\visceral\full_skeletons"
+    # bone_line = np.array([84,170,450]) - np.array([84,82,400]) # Legs
+    # bone_line = np.array([390,320,540]) - np.array([420,260,560]) # left pelvis bone
+    bone_line = np.array([-70. , 46. , 26.]) # right front pelvis bone
+    skeleton_paths = random.sample(glob.glob(os.path.join(skeleton_folder,"*.ply")), 10)
 
+    for file_name in skeleton_paths:
+        pcd = o3d.io.read_point_cloud(file_name)
+        o3d.visualization.draw_geometries([pcd])
+        pcd.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=10, max_nn=30))
+        pcd = remove_non_normals(pcd, bone_line)
+        check_full_bone(pcd, bone_line)
+
+def test3_clustering():
+    normal_rad = 10
+    source = o3d.io.read_point_cloud(r"D:\visceral\full_skeletons\102946_CT_Wb.ply")
+    numpy_source = np.asarray(source.points)
+    numpy_source = numpy_source[numpy_source[:,2]>300]
+    numpy_source = numpy_source[numpy_source[:, 2]<801]
+    resample = 0.75
+    source_index = np.random.randint(0,numpy_source.shape[0], int(numpy_source.shape[0]*resample))
+    source.points = o3d.utility.Vector3dVector(numpy_source[source_index])
+    source.paint_uniform_color([0.1, 0.1, 0.1])
+    source.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=normal_rad, max_nn=30))
+
+    bone_line = np.array([-70., 46., 26.])  # right front pelvis bone
+    source = remove_non_normals(source, bone_line)
+    get_bone_groups(source, bone_line)
 if __name__ == "__main__":
-    test1_consensus_class()
+    # test1_get_some_bones()
+    # test2_legs()
+    test3_clustering()
